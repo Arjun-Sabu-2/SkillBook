630fbd8fd0f065facbbbc4a5c71263d997df85de
ARJUN SABU <arjunsabu@MacBook-Pro.local>
2021-01-03 12:48:47 +0530

    Commit 2: add procfile and setup for heroku

--- app.js
+++ app.js
@@ -111,6 +111,12 @@ app.post('/login', function(req,res){
 
 });
 
-app.listen(3000, function(){
-  console.log('function is up and running at port 3000');
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
+  console.log('Server is up and running');
 });
